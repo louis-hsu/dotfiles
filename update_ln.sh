b2afb2e4da53dfbec6e4c0ab404effcdd31d1513
Louis Hsu <louis.shiu@gmail.com>
2015-03-11 17:43:16 +0800

    Update script to work with .(directory), such as .vim

--- update_ln.sh
+++ update_ln.sh
@@ -5,6 +5,10 @@
 # 1. If file in .dotfiles not exists in $HOME, create softlink
 # 2. If file in .dotfiles exists in %HOME, delete it from $HOME and create softlink
 # 3. If File in .dotfiles has softlink in $HOME, ignore
+#
+# Version: 1.1.0
+# Release date: 2015/0311
+# 1. Update script to accept .(directory), such as .vim
 
 DOTFILES_LOCATION=$HOME'/.dotfiles'
 LISTFILE="fileListTamp"
@@ -15,7 +19,7 @@ createSoftLink ()
 }
 
 cd $DOTFILES_LOCATION
-ls -alF $DOTFILE_LOCATION | awk '$9 ~ /^\./ && $9 !~ /\/$/ && $9 !~ /git/ {print $9}' > $LISTFILE
+\ls -al --ignore='.update*' $DOTFILE_LOCATION | awk '$9 ~ /^\./ && $9 !~ /\.$/ && $9 !~ /git/ {print $9}' > $LISTFILE
 
 while read -r line
 do

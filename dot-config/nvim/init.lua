048a950738326c36124dd8e62bb2f5cab96ac830
Louis Hsu <louis.shiu@gmail.com>
2024-05-17 16:25:31 +0800

    <feat> Clean-up undo files older than 30 days
    Update init.lua:
    1. Create function 'cleanup_undo_files' to clean-up undo files older
       than 30 days
    2. Leverage autocmd to execute 'cleanup_undo_files' when 'VimLeave'

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -4,3 +4,15 @@ require("plugins")
 require("options")
 --require("setup.spelling")
 require("mappings")
+
+-- Lua function to delete old undo files
+local function cleanup_undo_files()
+  local undo_dir = os.getenv("XDG_CONFIG_HOME") .. "/nvim/undo"
+  local cmd = "find " .. undo_dir .. " -type f -mtime +30 -exec rm {} \\;"
+  os.execute(cmd)
+end
+
+-- Run the cleanup function when exiting Neovim
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = cleanup_undo_files,
+})

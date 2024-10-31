79367c44df9e7c1029362243098966ac9e9fab70
Louis Hsu <louis.shiu@gmail.com>
2024-10-31 12:39:06 +0800

    <feat> Add 'auto-pairs' and 'lspkind' plugins
    1. Add 'auto-pairs' plugin to complete brackets automatically
    2. Add 'lspkind' plugin to beautify completion menu
    Also add <C-p>/<C-n> keymapping for item selection

--- dot-config/nvim/lua/plugins/completions.lua
+++ dot-config/nvim/lua/plugins/completions.lua
@@ -10,10 +10,50 @@ return {
 		},
 	},
 	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"onsails/lspkind.nvim",
+		},
 		config = function()
 			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local ts_utils = require("nvim-treesitter.ts_utils")
+
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local ts_node_func_parens_disabled = {
+				-- ecma
+				named_imports = true,
+				-- rust
+				use_declaration = true,
+			}
+
+			local default_handler = cmp_autopairs.filetypes["*"]["("].handler
+			cmp_autopairs.filetypes["*"]["("].handler = function(char, item, bufnr, rules, commit_character)
+				local node_type = ts_utils.get_node_at_cursor():type()
+				if ts_node_func_parens_disabled[node_type] then
+					if item.data then
+						item.data.funcParensDisabled = true
+					else
+						char = ""
+					end
+				end
+				default_handler(char, item, bufnr, rules, commit_character)
+			end
+
+			cmp.event:on(
+				"confirm_done",
+				cmp_autopairs.on_confirm_done({
+					sh = false,
+				})
+			)
 
 			cmp.setup({
 				snippet = {
@@ -30,10 +70,33 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
+				view = {
+					entries = { name = "custom", selection_order = "near_cursor" },
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = {
+							menu = 50,
+							abbr = 50,
+						},
+						ellipsis_char = "...",
+						show_labelDetails = true,
+						menu = {
+							buffer = "[Buf]",
+							nvim_lsp = "[LSP]",
+							luasnip = "[Snip]",
+						},
+					}),
+				},
+
 				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					-- ["<C-Space>"] = cmp.mapping.complete(), -- Conflict with macOS hotkey
+					["<C-r>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
@@ -43,8 +106,8 @@ return {
 					{ name = "luasnip" }, -- For luasnip users.
 					-- { name = 'ultisnips' }, -- For ultisnips users.
 					-- { name = 'snippy' }, -- For snippy users.
-				}, {
 					{ name = "buffer" },
+					{ name = "path" },
 				}),
 			})
 		end,

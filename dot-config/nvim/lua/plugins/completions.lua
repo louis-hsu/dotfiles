3026d9786b737ee26fdfc5edd5a1658e37e1c222
Louis Hsu <louis.shiu@gmail.com>
2024-10-31 16:17:30 +0800

    <feat> Add plugins to identify bracket/indent scope easier
    Add plutins:
    1. 'rainbow-delimiters'
    2. 'indent-blankline
    to make the scope of brackets/indents easier to be identify

--- dot-config/nvim/lua/plugins/completions.lua
+++ dot-config/nvim/lua/plugins/completions.lua
@@ -19,6 +19,7 @@ return {
 		dependencies = {
 			"onsails/lspkind.nvim",
 		},
+
 		config = function()
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
@@ -110,6 +111,64 @@ return {
 					{ name = "path" },
 				}),
 			})
+		end,
+	},
+	{
+		"hiphish/rainbow-delimiters.nvim",
+		dependencies = {
+			"lukas-reineke/indent-blankline.nvim",
+		},
+
+		config = function()
+			local rainbow = require("rainbow-delimiters")
+			local ibl = require("ibl")
+
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+
+			vim.g.rainbow = {
+				strategy = {
+					[""] = rainbow.strategy["global"],
+					vim = rainbow.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				priority = {
+					[""] = 110,
+					lua = 210,
+				},
+				highlight = highlight,
+			}
+			ibl.setup({
+				scope = {
+					enabled = true,
+					highlight = highlight,
+				},
+				indent = {
+					char = "▏",
+					smart_indent_cap = true,
+				},
+			})
+			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 		end,
 	},
 }

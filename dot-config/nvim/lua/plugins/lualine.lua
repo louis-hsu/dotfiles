d09ff89fbdb0f3271d4aadc22c386c952eea0254
Louis Hsu <louis.shiu@gmail.com>
2024-11-06 07:24:55 +0800

    <fix> Show relative path in section 'lualine_c'
    Update section 'lualine_c' with:
    'filename'
    'path = 1'
    to show filename with relative path

--- dot-config/nvim/lua/plugins/lualine.lua
+++ dot-config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,25 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 
-  config = function()
-    local l_dracula = require('lualine.themes.dracula')
-    l_dracula.normal.a.bg = '#8ac6f2'
-    l_dracula.insert.a.bg = '#95e454'
-    l_dracula.replace.a.bg = '#e5786d'
+	config = function()
+		local l_dracula = require("lualine.themes.dracula")
+		l_dracula.normal.a.bg = "#8ac6f2"
+		l_dracula.insert.a.bg = "#95e454"
+		l_dracula.replace.a.bg = "#e5786d"
 
-    require('lualine').setup({
-      options = {
-        theme = l_dracula,
-      }
-    })
-  end
+		require("lualine").setup({
+			options = {
+				theme = l_dracula,
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }

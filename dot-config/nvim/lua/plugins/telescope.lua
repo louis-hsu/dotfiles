3b53ca5ff306f8ca0420e472367865a7fca288d0
Louis Hsu <louis.shiu@gmail.com>
2024-11-06 10:18:10 +0800

    <style> Formatting by LSP
    Formatting by LSP

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,73 +1,73 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			--      local builtin = require("telescope.builtin")
+			--      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			--      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			--      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			--      vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
 
-      --local actions = require('telescope.actions')
-      local telescope = require("telescope")
-      telescope.setup({
-        defaults = {
-          vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '-C=0',
-          },
-          mappings = {
-            i = {
-              ["<esc>"] = "close",
-              ["<C-j>"] = "move_selection_next",
-              ["<C-k>"] = "move_selection_previous",
-            },
-            n = {
-              ["j"] = "move_selection_next",
-              ["k"] = "move_selection_previous",
-            },
-          },
-        }
-      })
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    -- This is your opts table
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-              -- even more opts
-            }
-            -- pseudo code / specification for writing custom displays, like the one
-            -- for "codeactions"
-            -- specific_opts = {
-            --   [kind] = {
-            --     make_indexed = function(items) -> indexed_items, width,
-            --     make_displayer = function(widths) -> displayer
-            --     make_display = function(displayer) -> function(e)
-            --     make_ordinal = function(e) -> string
-            --   },
-            --   -- for example to disable the custom builtin "codeactions" display
-            --      do the following
-            --   codeactions = false,
-            -- }
-          }
-        }
-      })
-      -- To get ui-select loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
-      require("telescope").load_extension("ui-select")
-    end
-  }
+			--local actions = require('telescope.actions')
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"-C=0",
+					},
+					mappings = {
+						i = {
+							["<esc>"] = "close",
+							["<C-j>"] = "move_selection_next",
+							["<C-k>"] = "move_selection_previous",
+						},
+						n = {
+							["j"] = "move_selection_next",
+							["k"] = "move_selection_previous",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		-- This is your opts table
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+						-- pseudo code / specification for writing custom displays, like the one
+						-- for "codeactions"
+						-- specific_opts = {
+						--   [kind] = {
+						--     make_indexed = function(items) -> indexed_items, width,
+						--     make_displayer = function(widths) -> displayer
+						--     make_display = function(displayer) -> function(e)
+						--     make_ordinal = function(e) -> string
+						--   },
+						--   -- for example to disable the custom builtin "codeactions" display
+						--      do the following
+						--   codeactions = false,
+						-- }
+					},
+				},
+			})
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }

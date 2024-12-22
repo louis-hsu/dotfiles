87908ecb31093b59dc8922e55ff78641409f9155
Louis Hsu <louis.shiu@gmail.com>
2024-12-22 20:16:40 +0800

    <refactor> Switch to new yazi plugin packages
    Switch to new yazi plugin packages due to original ones are obsolete
    without maintenance:
    1. ahkohd/eza-preview.yazi
    2. boydaihungst/mediainfo.yazi
    
    And update 'init.lua'/'package.toml' accordingly due to new repo and/or
    new available options.

--- dot-config/yazi/init.lua
+++ dot-config/yazi/init.lua
@@ -1,2 +1,12 @@
 require("git"):setup()
-require("eza-preview"):setup()
+require("eza-preview"):setup({
+	-- Determines the directory depth level to tree preview (default: 3)
+	level = 3,
+	-- Whether to follow symlinks when previewing directories (default: false)
+	follow_symlinks = true,
+	-- Whether to show target file info instead of symlink info (default: false)
+	dereference = true,
+})
+
+-- Or use default settings with empty table
+require("eza-preview"):setup({})

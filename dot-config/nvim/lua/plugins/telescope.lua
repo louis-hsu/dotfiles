8a1076f1e94d48928a7796c274ca0858cf639d8e
Louis Hsu <louis.shiu@gmail.com>
2024-12-22 20:57:50 +0800

    <refactor> Change layout of 'telescope' picker
    Change layout of 'telescope' picker:
    1. Move 'query pane' on the top
    2. Change layout to vertical when window size < 100 (and move preview
    pane to the top, query pane in the middle)

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
+		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+				-- build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+				build = "make",
 			},
 			-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			"nvim-tree/nvim-web-devicons",
@@ -22,36 +23,64 @@ return {
 
 			local actions = require("telescope.actions")
 			local telescope = require("telescope")
+
+			local function set_telescope_layout()
+				local columns = vim.o.columns
+				local layout
+
+				if columns > 120 then
+					layout = "horizontal"
+				elseif columns > 100 then
+					layout = "flex"
+				else
+					layout = "vertical"
+				end
+
+				telescope.setup({
+					defaults = {
+						layout_config = {
+							-- For default layout
+							horizontal = {
+								width = 0.7, -- 80% of total width
+								height = 0.9, -- 90% of total height
+								preview_width = 0.6, -- 60% of width for preview pane
+								results_width = 0.4, -- 40% of width for results pane
+								prompt_position = "top",
+							},
+							-- For vertical layout
+							vertical = {
+								width = 0.7,
+								height = 0.9,
+								preview_height = 0.5, -- 50% of height for preview pane
+								prompt_position = "top",
+								-- results_height = 0.5, -- Comment out since it causes error
+							},
+							-- -- For dropdown layout (when window is small)
+							-- cursor = {
+							-- 	width = 0.5,
+							-- 	height = 0.7,
+							-- 	preview_cutoff = 40, -- Preview will be hidden if results less than this
+							-- },
+							flex = {
+								flip_columns = 100,
+								prompt_position = "top",
+							},
+						},
+						sorting_strategy = "ascending",
+						layout_strategy = layout,
+					},
+				})
+			end
+
+			-- Create an autocommand to reset layout when window is resized
+			vim.api.nvim_create_autocmd({ "VimResized" }, {
+				callback = set_telescope_layout,
+			})
+			-- Initial setup
+			set_telescope_layout()
+
 			telescope.setup({
 				defaults = {
-					layout_config = {
-						-- For default layout
-						horizontal = {
-							width = 0.7,  -- 80% of total width
-							height = 0.9, -- 90% of total height
-							preview_width = 0.6, -- 60% of width for preview pane
-							results_width = 0.4, -- 40% of width for results pane
-						},
-
-						-- For vertical layout
-						vertical = {
-							width = 0.5,
-							height = 0.9,
-							preview_height = 0.5, -- 50% of height for preview pane
-							results_height = 0.5, -- 50% of height for results pane
-						},
-
-						-- For dropdown layout (when window is small)
-						cursor = {
-							width = 0.5,
-							height = 0.7,
-							preview_cutoff = 40, -- Preview will be hidden if results less than this
-						},
-					},
-
-					-- You can also set a specific layout strategy
-					layout_strategy = "horizontal", -- or 'vertical', 'cursor'
-
 					vimgrep_arguments = {
 						"rg",
 						"--color=never",

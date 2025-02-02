43c130d845fb660dfc2feb45d8d354e5b4b84606
Louis Hsu <louis.shiu@gmail.com>
2025-02-02 21:41:04 +0800

    <fix> Remove buffer/scratchpad feature to fix undotree feature
    Remove buffer/scratchpad feature to make undotree work correctly when
    enabled (stay at the right place with right width)
    Also make 2 buffers are not accessible to make moving simple
    
    TODO:
    Without breaking undotree feature
    1. Bring right scratchpad back, and
    2. Make left buffer not accesible

--- dot-config/nvim/lua/plugins/no-neck-pain.lua
+++ dot-config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,5 +1,6 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
+	version = "*",
 
 	config = function()
 		-- Create scratchPad directory
@@ -11,21 +12,36 @@ return {
 		local nopain = require("no-neck-pain")
 		nopain.setup({
 			width = 120,
-			buffers = {
-				right = {
-					enabled = true,
-					scratchPad = {
-						enabled = true,
-						pathToFile = vim.fn.stdpath("data") .. "/scratchPad/scratchPad.md",
-					},
-				},
-				left = {
-					enabled = true,
+			minSideBufferWidth = 20,
+
+			autocmds = {
+				skipEnteringNoNeckPainBuffer = true,
+			},
+			-- Decide to remove buffer/scratchpad function since
+			-- 'skipEnteringNoNeckPainBuffer' doesn't work on one side only, and this
+			-- feature is more important than scrachpad -- Louis 2024/1217
+			--
+			-- buffers = {
+			-- 	right = {
+			-- 		-- enabled = true,
+			-- 		scratchPad = {
+			-- 			enabled = true,
+			-- 			pathToFile = vim.fn.stdpath("data") .. "/scratchPad/scratchPad.md",
+			-- 		},
+			-- 	},
+			-- 	left = {
+			-- 		enabled = false,
+			-- 	},
+			-- },
+
+			integrations = {
+				undotree = {
+					width = 24,
+					position = "right",
 				},
 			},
 		})
 	end,
 
 	vim.keymap.set("n", "<leader>np", "<Cmd>NoNeckPain<CR>"),
-	-- vim.keymap.set("n", "<leader>ns", "<Cmd>NoNeckPainScratchPad<CR>"),
 }

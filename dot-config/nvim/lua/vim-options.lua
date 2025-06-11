f7a9adf43bf2976ac10476c8248bee5c4b420d2c
Louis Hsu <louis.shiu@gmail.com>
2025-06-11 15:05:15 +0800

    <fix> Linter formatting
    Linter auto formatting

--- dot-config/nvim/lua/vim-options.lua
+++ dot-config/nvim/lua/vim-options.lua
@@ -4,29 +4,29 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
 vim.opt.smarttab = true
 
-vim.opt.number = true             -- Show line number
-vim.opt.relativenumber = true     -- Show relative number
+vim.opt.number = true -- Show line number
+vim.opt.relativenumber = true -- Show relative number
 vim.opt.backspace = { "indent", "eol", "start" }
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard for copy/paste
 vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.scrolloff = 4             -- At least 4 lines visible at top or bottom
-vim.opt.sidescrolloff = 8         -- At least 8 columns visible at right or left
-vim.opt.splitbelow = true         -- Put new windows below current
-vim.opt.splitright = true         -- Put new windows right of current
+vim.opt.scrolloff = 4 -- At least 4 lines visible at top or bottom
+vim.opt.sidescrolloff = 8 -- At least 8 columns visible at right or left
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
 vim.opt.termguicolors = true
 
 -- For search setup
 vim.opt.ignorecase = true
-vim.opt.smartcase = true     -- Do not ignore case which capitals
-vim.opt.incsearch = true     -- Show the match while typing
+vim.opt.smartcase = true -- Do not ignore case which capitals
+vim.opt.incsearch = true -- Show the match while typing
 vim.opt.inccommand = "split" -- Get a preview of replacements
 
 -- For line auto-wrap
-vim.opt.wrap = true               -- Optionally enable visual wrapping
-vim.opt.textwidth = 80            -- Set length to 80
+vim.opt.wrap = true -- Optionally enable visual wrapping
+vim.opt.textwidth = 80 -- Set length to 80
 --vim.opt.colorcolumn = "80"
 vim.opt.formatoptions:append("t") -- Enable automatic text wrapping while typing
-vim.opt.linebreak = true          -- Stop words being broken on wrap
+vim.opt.linebreak = true -- Stop words being broken on wrap
 
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"

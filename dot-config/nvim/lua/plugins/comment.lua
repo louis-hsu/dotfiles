8545018475432a8b79af9cb5daa402e27d3850f7
Louis Hsu <louis.shiu@gmail.com>
2025-06-16 16:49:39 +0800

    <feat> Install new plugin 'which-key'
    Install new plugin 'which-key'
    1. Show configured keymappings when pressing <leader> key
    2. Remove 'gc'/'gb' mapping then add them back via 'which-key' to solve
    overlapping/conflict between 'comment.lua' and nvim native commenting
    feature.

--- dot-config/nvim/lua/plugins/comment.lua
+++ dot-config/nvim/lua/plugins/comment.lua
@@ -6,5 +6,17 @@ return {
 		comment.setup({
 			extra = { above = "gca", below = "gcb", eol = "gce" },
 		})
+
+		-- https://github.com/numToStr/Comment.nvim/issues/483
+		-- Temporary solution to disable nvim native commenting keymapping
+		-- I also don't need to much fancy commenting skills
+		-- Louis 2025/0613
+		vim.keymap.del("n", "gc")
+		vim.keymap.del("n", "gb")
+		local wk = require("which-key")
+		wk.add({
+			{ "gb", group = "Comment toggle blockwise" },
+			{ "gc", group = "Comment toggle linewise" },
+		})
 	end,
 }

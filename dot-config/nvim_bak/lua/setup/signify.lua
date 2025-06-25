96f75445a7f068b1628a47cdb80549cfa4475eee
Louis Hsu <louis.shiu@gmail.com>
2025-06-25 15:26:24 +0800

    <fix> Remove 'nvim_bak'
    Remove back-up directory 'nvim_bak'

--- dot-config/nvim_bak/lua/setup/signify.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd([[
-set updatetime=500
-
-highlight SignifySignAdd    guifg=#a3be8c gui=bold
-highlight SignifySignDelete guifg=#bf616a gui=bold
-highlight SignifySignChange guifg=#ebcb8b gui=bold
-
-]])

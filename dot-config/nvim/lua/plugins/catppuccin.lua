1f7596e1b72669651779bfdb9fd4430b8ff97e9a
Louis Hsu <louis.shiu@gmail.com>
2025-08-26 15:07:54 +0800

    <fix> Cleanup colortheme setup
    Cleanup colortheme 'catppuccin' color customization setup

--- dot-config/nvim/lua/plugins/catppuccin.lua
+++ dot-config/nvim/lua/plugins/catppuccin.lua
@@ -8,12 +8,12 @@ return {
 			flavor = "mocha",
 			transparent_background = true,
 
-			-- ---@diagnostic disable-next-line: unused-local
-			-- custom_highlights = function(colors)
-			-- 	return {
-			-- 		Comment = { fg = "#5c6370" },
-			-- 	}
-			-- end,
+			---@diagnostic disable-next-line: unused-local
+			custom_highlights = function(colors)
+				return {
+					Comment = { fg = "#5c6370" },
+				}
+			end,
 		})
 		vim.cmd.colorscheme("catppuccin")
 		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f28fad", bold = true })

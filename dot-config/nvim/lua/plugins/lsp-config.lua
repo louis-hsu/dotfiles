48143ab211732e132323a1a550527a67a6e6637b
Louis Hsu <louis.shiu@gmail.com>
2026-02-13 23:20:32 +0800

    <fix> Fix phase-out issue in 'nvim-lspconfig'
    In new 'nvim-lspconfig':
    1. Traditional calling 'require('lspconfig').server_name.setup({})' is
       phased out
    2. New syntax is 'vim.lsp.config('server_name')'
    Update to new syntax

--- dot-config/nvim/lua/plugins/lsp-config.lua
+++ dot-config/nvim/lua/plugins/lsp-config.lua
@@ -18,7 +18,7 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+			-- local lspconfig = require("lspconfig")
 			local on_attach = function(client, bufnr)
 				--vim.keymap.set("n", "<leader>ge", vim.diagnostic.open_float, { noremap = true, silent = true })
 				vim.keymap.set(
@@ -76,7 +76,8 @@ return {
 			end
 
 			local root_pattern = require("utilis.root_pattern")
-			require("lspconfig").lua_ls.setup({
+			-- require("lspconfig").lua_ls.setup({
+			vim.lsp.config("lua_ls", {
 				root_dir = function(fname)
 					local root = root_pattern.closest_root_pattern({ ".luarc.json", "init.lua", ".git" })(fname)
 					-- print("DEBUG: root_dir is", root)
@@ -107,32 +108,38 @@ return {
 				capabilities = capabilities,
 				on_attach = on_attach,
 			})
-			lspconfig.ts_ls.setup({
+			-- lspconfig.ts_ls.setup({
+			vim.lsp.config("ts_ls", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "typescript", "javascript" },
 			})
-			lspconfig.bashls.setup({
+			-- lspconfig.bashls.setup({
+			vim.lsp.config("bashls", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "sh" },
 			})
-			lspconfig.jdtls.setup({
+			-- lspconfig.jdtls.setup({
+			vim.lsp.config("jdtls", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "java" },
 			})
-			lspconfig.ruff.setup({
+			-- lspconfig.ruff.setup({
+			vim.lsp.config("ruff", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "python" },
 			})
-			lspconfig.pyright.setup({
+			-- lspconfig.pyright.setup({
+			vim.lsp.config("pyright", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "python" },
 			})
-			lspconfig.kotlin_language_server.setup({
+			-- lspconfig.kotlin_language_server.setup({
+			vim.lsp.config("kotlin", {
 				capabilities = capabilities,
 				on_attach = on_attach,
 				filetypes = { "kotlin" },
